c672e22c2cafbb9eaf42dd2abba9d64731144dfd
nattto66 <naoto.takizawa1@gmail.com>
2019-02-27 20:32:19 +0900

    Change a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-# ページごとの完全なタイトルを返します　#コメント行
-  def full_title(page_title = '')　#メソッド定義とオプション引数
+# ページごとの完全なタイトルを返します #コメント行
+  def full_title(page_title = '') #メソッド定義とオプション引数
     base_title = "Ruby on Rails Tutorial Sample App" #変数への代入
-    if page_title.empty?　#論理値テスト
-      base_title　#暗黙の戻り値
+    if page_title.empty? #論理値テスト
+      base_title #暗黙の戻り値
     else
-      page_title + "|" + base_title　#戻り値の場合
+      page_title + "|" + base_title #戻り値の場合
     end
   end
 end
