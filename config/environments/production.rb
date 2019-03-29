d4e7c87d1ae7aa497e7de32be8f49225dbfb96ab
nattto66 <naoto.takizawa1@gmail.com>
2019-03-29 09:20:27 +0900

    change environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://stormy-hamlet-49067.herokuapp.com/'
+  host = 'stormy-hamlet-49067.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
    :address        => 'smtp.sendgrid.net',
