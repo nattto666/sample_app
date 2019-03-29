55097bbadbe76e6bb4ee65cdf15a598613e4e7c2
nattto66 <naoto.takizawa1@gmail.com>
2019-03-29 09:02:08 +0900

    change environment/production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,18 +65,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.actioin_mailer.raise_delivery_errors = true
-  config.actioin_mailer.delivery_method = :smtp
-  host = 'https://stormy-hamlet-49067.herokuapp.com'
-  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = '<your heroku app>.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_starttls_auto => true
+   :address        => 'smtp.sendgrid.net',
+   :port           => '587',
+   :authentication => :plain,
+   :user_name      => ENV['SENDGRID_USERNAME'],
+   :password       => ENV['SENDGRID_PASSWORD'],
+   :domain         => 'heroku.com',
+   :enable_starttls_auto => true
   }
 
 
